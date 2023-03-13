a9f38f200c14394e1f07a4ef92e7f85fb71b21c1
vin3ntKing <vin3ntKing@gmail.com>
2023-03-13 17:02:35 -0400

    3/13 commit

--- client/client.js
+++ client/client.js
@@ -1,31 +1,45 @@
-const form = document.querySelector('form')
-const loadingElement = document.querySelector('.loading')
-const API_URL = 'http://localhost:8000/tweet';
+const form = document.querySelector("form");
+const loadingElement = document.querySelector(".loading");
+const API_URL = "http://localhost:8000/tweet";
 
-loadingElement.style.display = 'none';
+const listAllTweets = () => {
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((allTweets) => {
+      console.log(allTweets);
+    });
+};
+
+loadingElement.style.display = "none";
+
+listAllTweets();
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const name = formData.get('name');
-    const content = formData.get('content');
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get("name");
+  const content = formData.get("content");
 
-    const tweet = {
-        name,
-        content
-    };
+  const tweet = {
+    name,
+    content,
+  };
+  console.log(tweet);
+  form.style.display = "none";
+  loadingElement.style.display = "";
 
-    console.log(tweet);
-    form.style.display = 'none';
-    loadingElement.style.display = '';
-
-    fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(tweet),
-        headers: {
-            'content-type' : 'application/json'
-
-        }
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(tweet),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((createdTweet) => {
+      console.log(createdTweet);
+      form.reset();
+      form.style.display = "";
+      loadingElement.style.display = "none";
     });
-    
-})
+});
