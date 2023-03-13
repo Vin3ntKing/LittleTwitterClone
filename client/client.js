896a478b2d5687e481bef76ab56ec39298e0ca15
vin3ntKing <vin3ntKing@gmail.com>
2023-03-13 18:52:44 -0400

    final commit

--- client/client.js
+++ client/client.js
@@ -1,12 +1,33 @@
 const form = document.querySelector("form");
 const loadingElement = document.querySelector(".loading");
+const tweetsElement = document.querySelector(".tweets")
 const API_URL = "http://localhost:8000/tweet";
 
 const listAllTweets = () => {
+  tweetsElement.innerHTML = "";
   fetch(API_URL)
     .then((response) => response.json())
     .then((allTweets) => {
       console.log(allTweets);
+      allTweets.reverse();
+      allTweets.forEach((tweet) => {
+        const div = document.createElement("div");
+
+        const header = document.createElement("h3");
+        header.textContent = tweet.name;
+
+        const content = document.createElement("p");
+        content.textContent = tweet.content;
+
+        const date = document.createElement("small");
+        date.textContent = new Date(tweet.created);
+
+        div.appendChild(header);
+        div.appendChild(content);
+        div.appendChild(date);
+
+        tweetsElement.appendChild(div);
+      });
     });
 };
 
@@ -41,5 +62,6 @@ form.addEventListener("submit", (event) => {
       form.reset();
       form.style.display = "";
       loadingElement.style.display = "none";
+      listAllTweets();
     });
 });
